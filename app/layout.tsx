6d869af1156b2a2ca9b0fe7f4539590ebb71c2ad
Samarkun <samarkun4@gmail.com>
2025-07-16 09:29:00 +0530

    User Dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import AuthProvider from "@/app/context/AuthProvider";
+import { Toaster } from "@/components/ui/sonner"
 import Navbar from "@/components/Navbar";
+import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,15 +28,15 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
-
-      </body>
-
+      <AuthProvider >
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {/* <Navbar /> */}
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
 
     </html>
   );
