f147b4dfd19a82bd0e0c5e97dc968568cf137096
Samarkun <samarkun4@gmail.com>
2025-07-17 20:53:13 +0530

    fix the ui of login page sign-up issue

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -98,9 +98,9 @@ const signInPage = () => {
                 </Form>
                 <div className="text-center mt-4">
                     <p>
-                        Already a member? {''}
-                        <Link href='/sign-in' className="text-blue-600 hover:text-blue-800">
-                            Sign in
+                        Create new account {''}
+                        <Link href='/sign-up' className="text-blue-600 hover:text-blue-800">
+                            Sign up
                         </Link>
                     </p>
                 </div>
